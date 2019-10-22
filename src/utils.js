ddf81c9e916c893c4d7516812280c750390e7eae
Alex Carpenter <alex.carpenter@nationbuilder.com>
2019-10-22 19:51:08 -0400

    Cleanup utils

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,3 @@
-// https://davidwalsh.name/javascript-debounce-function
-export function debounce(fn, wait, immediate) {
-  let timeout;
-  return () => {
-    const context = this;
-    const args = arguments;
-    const later = () => {
-      timeout = null;
-      if (!immediate) fn.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) fn.apply(context, args);
-  };
-}
-
 export function setAttributes(el, attrs) {
   for (var key in attrs) {
     el.setAttribute(key, attrs[key]);
@@ -41,14 +24,3 @@ export function visuallyHide(element) {
   element.setAttribute('aria-hidden', 'true');
   element.setAttribute('tabindex', '-1');
 }
-
-export const keyCodes = {
-  esc: 27,
-  tab: 9,
-  upArrow: 38,
-  rightArrow: 39,
-  downArrow: 40,
-  leftArrow: 37,
-  home: 36,
-  end: 35,
-};
